d2190c7bbc59456ee1a04d3db97bd6db051cf40a
Aprendiz <Aprendiz@BOGDFPCGMF4148.FormacionSena.RED>
2024-03-05 17:19:21 -0500

    sena

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
+import os 
+besadir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/citas_2826502'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(besadir,  "database.db")
     SQLALCHEMY_TRACK_NOTIFICATIONS = True
     
     
